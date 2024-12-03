53dd52e905bd6cbb4fd4f2bc6b2010bbd936b4ba
Tako <tako.renraku1@gmail.com>
2024-12-03 12:16:06 +0900

    update

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -4,12 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from "./components/Header";
 import Contents from "./components/Contents";
+import Fotter from "./components/Footer";
 
 const Main = () => {
   return (
     <div>
       <Header />
         {/* <Contents /> */}
+        <Footer />
     </div>
   );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './Main';
+import Main from './Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Main />
   </React.StrictMode>
 );
