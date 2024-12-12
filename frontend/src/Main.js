1b23620400ba3d71d85e9c7ff7499e2bffbcc2a8
Tako <tako.renraku1@gmail.com>
2024-12-12 16:54:03 +0900

    some fix - あしたがんばる

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import SignIn from './Sign_in';
-import SignUp from './Sign_up';
+import SignIn from './pages/Sign_in';
+import SignUp from './pages/Sign_up';
 
 const Main = () => {
   return (
