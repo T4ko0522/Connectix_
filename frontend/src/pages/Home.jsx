7fc555db84788e686e9c141c70c505142e9d07fd
Tako <tako.renraku1@gmail.com>
2025-02-12 16:56:55 +0900

    Update Home.jsx Add Footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,6 +10,7 @@ import CardContent from '@mui/material/CardContent';
 import Avatar from "@mui/material/Avatar";
 import { YouTube, Twitter } from "@mui/icons-material";
 import InstagramIcon from '@mui/icons-material/Instagram';
+import Footer from "../components/Footer.jsx";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -170,6 +171,7 @@ export default function Home() {
           </Box>
         </Box>
       </Container>
+      <Footer />
     </Box>
   );
 }
