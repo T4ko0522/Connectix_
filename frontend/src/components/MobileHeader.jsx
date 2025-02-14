cb306488f3cc495b16ad7712301ce39e943ec15c
Tako <tako.renraku1@gmail.com>
2025-02-14 20:36:43 +0900

    import React fix warning

--- frontend/src/components/MobileHeader.jsx
+++ frontend/src/components/MobileHeader.jsx
@@ -1,5 +1,4 @@
-import React from "react"
-import { useState, useEffect } from "react"
+import React, { useState, useEffect} from "react"
 import { useNavigate } from "react-router-dom"
 import AppBar from "@mui/material/AppBar"
 import Box from "@mui/material/Box"
