2dbc095889b635d01131ad1444235f7c5135ba60
Tako <tako.renraku1@gmail.com>
2025-02-17 09:53:50 +0900

    Fix fetch path

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -64,7 +64,7 @@ export default function Dashboard() {
   const fetchUsername = async (token) => {
     try {
       // TODO
-      // const response = await fetch("http://localhost:3522/api/auth/usersname", {
+      // const response = await fetch("http://localhost:3522/api/auth/username", {
       const response = await fetch("https://connectix-server.vercel.app/api/auth/username", {
         method: "GET",
         headers: {
