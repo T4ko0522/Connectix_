333b6b9d17b7a4eaa63df77608e8ed16e6e4d077
Tako <tako.renraku1@gmail.com>
2025-02-18 15:33:38 +0900

    グラデーションの色変えた

--- frontend/src/pages/RestPassword.jsx
+++ frontend/src/pages/RestPassword.jsx
@@ -8,7 +8,7 @@ const GradientBackground = styled(Box)({
   minHeight: "100vh",
   display: "flex",
   alignItems: "center",
-  background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+  background: "linear-gradient(135deg, #1e202c 0%, #6e729d 100%)",
 })
 
 const StyledCard = styled(Card)({
